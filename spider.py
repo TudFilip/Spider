b4be5c07dfb9599a7ad74133bca7dcee9a5241f9
TudFilip <filiptudormihail@yahoo.com>
2022-12-01 21:46:49 +0200

    rename method

--- spider.py
+++ spider.py
@@ -183,10 +183,10 @@ class SpiderTool(tkinter.Tk):
             self.search_button.destroy()
             self.__create_search_button()
 
-    def run(self):
+    def run_spider(self):
         self.mainloop()
 
 
 if __name__ == '__main__':
     app = SpiderTool()
-    app.run()
+    app.run_spider()
